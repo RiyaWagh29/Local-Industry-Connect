40a87d85e8b452aa80eb6740fc50067ef675ea23
Riya Wagh <riyawagh2909@gmail.com>
2026-04-14 15:46:44 +0530

    fix railway

--- server/config/env.js
+++ server/config/env.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  port: process.env.PORT || 5000,
+  port: process.env.PORT || 3000,
   db: process.env.MONGO_URI,
   jwt: process.env.JWT_SECRET || 'fallback_secret',
 };
